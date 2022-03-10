087351e70e2ac1ab1fe58c22a4bb5f6663fdb2c4
LAPTOP-CLNLDFTF\jacks <jacksonhacker2000@gmail.com>
2022-03-10 12:12:34 -0500

    html shenanigans

--- popup.js
+++
@@ -1,9 +0,0 @@
-const button1 = document.getElementById("button1");
-const buttonOptions = document.getElementById("buttonDiv");
-const selectedClassName = "current"
-
-button1.addEventListener("click", () => {
-    button1.classList.toggle(selectedClassName);
-    buttonOptions.classList.toggle(selectedClassName);
-    alert("You clicked the button!");
-});
