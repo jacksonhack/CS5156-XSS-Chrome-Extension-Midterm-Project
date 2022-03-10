801c2f4bd741e4ce1efe08a5a64fb0f89c8bdf99
LAPTOP-CLNLDFTF\jacks <jacksonhacker2000@gmail.com>
2022-03-10 11:16:16 -0500

    simple js test and url/cookie grabbing

--- eventPage.js
+++ eventPage.js
@@ -4,13 +4,20 @@ var simplehtmlMenuItem = {
     "contexts": ["editable"]
 };
 
+var simpleJSMenuItem = {
+    "id": "injectSimpleJS",
+    "title": "Inject Simple JS Alert",
+    "contexts": ["editable"]
+};
+
 var iframeMenuItem = {
     "id": "injectIFrame",
-    "title": "Inject IFrame HTML",
+    "title": "Inject IFrame HTML that tries to grab URL and cookies",
     "contexts": ["editable"]
 };
 
 chrome.contextMenus.create(simplehtmlMenuItem);
+chrome.contextMenus.create(simpleJSMenuItem);
 chrome.contextMenus.create(iframeMenuItem);
 
 // on click, fill editable with the script
@@ -18,10 +25,12 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
     var tArea = document.createElement('textarea'), _frame = null;
     document.body.appendChild(tArea);
     if (info.menuItemId === "injectIFrame") {
-        tArea.value = '<iframe onload="alert(\'I AM VULNERABLE TO CROSS SITE SCRIPTING\')" style="display:none"></iframe>';
+        tArea.value = '<iframe onload="alert(\'I AM VULNERABLE TO JS INJECTION\\nMY URL IS: \' + document.URL +\'\\nMY COOKIES ARE: \' + document.cookies)" style="display:none"></iframe>';
     }
     else if (info.menuItemId === "injectSimplehtml") {
-        tArea.value = '<h1 style="background-color:tomato;">I AM VULNERABLE TO CROSS SITE SCRIPTING</h1>';
+        tArea.value = '<h1 style="background-color:tomato;">I AM VULNERABLE TO SIMPLE HTML INJECTION</h1>';
+    } else if (info.menuItemId === "injectSimpleJS") {
+        tArea.value = '<script>alert("I AM VULNERABLE TO SIMPLE JS INJECTION")</script>';
     }
     tArea.focus();
     tArea.select();
